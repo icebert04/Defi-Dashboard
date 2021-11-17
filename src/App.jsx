b21ab47c85c8b0da44b65538d5a4983911efd9a7
icebert04 <icebert04@gmail.com>
2021-11-17 15:51:07 +0800

    css update v1

--- src/App.jsx
+++ src/App.jsx
@@ -27,15 +27,15 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     fontFamily: "Roboto, sans-serif",
-    color: "#041836",
-    marginTop: "130px",
-    padding: "10px",
+    marginTop: "100px",
+    color: "#ffffff",
   },
   header: {
     position: "fixed",
     zIndex: 1,
     width: "100%",
     background: "#a30000",
+    color: "#ffffff",
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
@@ -67,14 +67,16 @@ const App = ({ isServerInfo }) => {
         <Header style={styles.header}>
           <Logo />
           <Menu
-            theme="light"
-            mode="horizontal"
+            theme="dark"
+            mode="inline"
             style={{
               display: "flex",
-              fontSize: "17px",
+              fontSize: "18.5px",
               fontWeight: "500",
               width: "100%",
               justifyContent: "center",
+              background: "#a30000",
+              color: "#fffff",
             }}
             defaultSelectedKeys={["quickstart"]}
           >
@@ -96,12 +98,12 @@ const App = ({ isServerInfo }) => {
           </Menu>
           <div style={styles.headerRight}>
             <Chains />
-            <TokenPrice
+            {/* <TokenPrice
               address="0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
               chain="eth"
               image="https://cloudflare-ipfs.com/ipfs/QmXttGpZrECX5qCyXbBQiqgQNytVGeZW5Anewvh2jc4psg/"
               size="40px"
-            />
+            /> */}
             <NativeBalance />
             <Account />
           </div>
@@ -166,14 +168,14 @@ export const Logo = () => (
     viewBox="0 0 30 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg">
-    <path
+    {/* <path
       d="M0 10C0 4.47715 4.47715 0 10 0H20C25.5228 0 30 4.47715 30 10V20C30 25.5228 25.5228 30 20 30H10C4.47715 30 0 25.5228 0 20V10Z"
       fill="#E84142"
     />
     <path
       d="M20.2914 15.3898C20.8111 14.4921 21.6497 14.4921 22.1693 15.3898L25.4056 21.0709C25.9252 21.9685 25.5 22.7008 24.4607 22.7008H17.941C16.9134 22.7008 16.4882 21.9685 16.9961 21.0709L20.2914 15.3898ZM14.0315 4.45277C14.5512 3.55513 15.378 3.55513 15.8977 4.45277L16.6182 5.75198L18.3189 8.74017C18.7323 9.59056 18.7323 10.5945 18.3189 11.4449L12.6142 21.3307C12.0945 22.1339 11.2323 22.6417 10.2756 22.7008H5.53942C4.50005 22.7008 4.07485 21.9803 4.59454 21.0709L14.0315 4.45277Z"
       fill="white"
-    />
+    /> */}
   </svg>
   </div>
 );
--- src/components/ERC20Balance.jsx
+++ src/components/ERC20Balance.jsx
@@ -1,9 +1,8 @@
-import { useMoralis } from "react-moralis";
+// import { useMoralis } from "react-moralis";
 import { useERC20Balance } from "../hooks/useERC20Balance";
 import { Skeleton, Table } from "antd";
-import { getEllipsisTxt } from "../helpers/formatters";
+// import { getEllipsisTxt } from "../helpers/formatters";
 import { useWeb3React } from "@web3-react/core";
-import axios from "axios";
 import useSWR from "swr";
 const styles = {
   title: {
@@ -13,66 +12,68 @@ const styles = {
 };
 function ERC20Balance(props) {
   const { assets } = useERC20Balance(props);
-  const { Moralis } = useMoralis();
-    const fetcher = url => axios.get(url).then(res => res.data)
+  // const { Moralis } = useMoralis();
     const { account, library } = useWeb3React();
     const { data: balance } = useSWR(['getBalance', account, 'latest'], {
-      fetcher: fetcher(library),
+    return:library
     })
+  if(!balance) {
+    return <div>No balance</div>
+  }
   
-  
-  const columns = [
-    {
-      title: "",
-      dataIndex: "logo",
-      key: "logo",
-      render: (logo) => (
-        <img
-          src={logo || "https://etherscan.io/images/main/empty-token.png"}
-          alt="nologo"
-          width="28px"
-          height="28px"
-        />
-      ),
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-      render: (name) => name,
-    },
-    {
-      title: "Symbol",
-      dataIndex: "symbol",
-      key: "symbol",
-      render: (symbol) => symbol,
-    },
-    {
-      title: "Balance",
-      dataIndex: "balance",
-      key: "balance",
-      render: (value, item) =>
-        parseFloat(Moralis.Units.FromWei(value, item.decimals).toFixed(6)),
-    },
-    {
-      title: "Address",
-      dataIndex: "token_address",
-      key: "token_address",
-      render: (address) => getEllipsisTxt(address, 5),
-    },
-  ];
+  // const columns = [
+  //   {
+  //     title: "",
+  //     dataIndex: "logo",
+  //     key: "logo",
+  //     render: (logo) => (
+  //       <img
+  //         src={logo || "https://etherscan.io/images/main/empty-token.png"}
+  //         alt="nologo"
+  //         width="28px"
+  //         height="28px"
+  //       />
+  //     ),
+  //   },
+  //   {
+  //     title: "Name",
+  //     dataIndex: "name",
+  //     key: "name",
+  //     render: (name) => name,
+  //   },
+  //   {
+  //     title: "Symbol",
+  //     dataIndex: "symbol",
+  //     key: "symbol",
+  //     render: (symbol) => symbol,
+  //   },
+  //   {
+  //     title: "Balance",
+  //     dataIndex: "balance",
+  //     key: "balance",
+  //     render: (value, item) =>
+  //       parseFloat(Moralis.Units.FromWei(value, item.decimals).toFixed(6)),
+  //   },
+  //   {
+  //     title: "Address",
+  //     dataIndex: "token_address",
+  //     key: "token_address",
+  //     render: (address) => getEllipsisTxt(address, 5),
+  //   },
+  // ];
 
   return (
+  <>
     <div style={{ width: "65vw", padding: "15px" }}>
       <h1 style={styles.title}>💰Token Balances</h1>
-      <Skeleton loading={!assets}>
+      <Skeleton loading={assets}>
         <Table
           dataSource={assets}
-          columns={columns}
-          Balance={balance.toString()}
         />
       </Skeleton>
     </div>
+    <div>Balance: {balance.toString()}</div>
+  </>
   );
 }
 export default ERC20Balance;
